ebe5d4b81665a0ddeacebcc957ec67af41978068
AnastaciaVolkova <polarbolide@gmail.com>
2021-01-06 22:53:58 +0100

    feat: Count maximum number of objects in one document

--- utils/get_statistic.py
+++ utils/get_statistic.py
@@ -47,14 +47,17 @@ def get_statistics(xml_dir):
     truncated = set()
     difficult = set()
 
+    max_num_objects = 0
     while data_q.qsize() != 0:
         objs_in_xml = data_q.get()
+        max_num_objects = max(max_num_objects, len(objs_in_xml))
         for i in objs_in_xml:
             name.add(i.name)
             pose.add(i.pose)
             truncated.add(i.truncated)
             difficult.add(i.difficult)
 
+    print("max object number %i" % max_num_objects)
     print("name %s" % name)
     print("pose %s" % pose)
     print("trancated %s" % truncated)
