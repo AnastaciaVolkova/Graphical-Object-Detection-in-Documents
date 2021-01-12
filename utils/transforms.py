b56c6a1dce4ee99b0dfc662c3531db2d14cf5c4a
AnastaciaVolkova <polarbolide@gmail.com>
2021-01-12 20:14:33 +0100

    feat: Create Crop transform

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 
 
 class ToNormGreyFloat:
@@ -40,3 +41,28 @@ class Resize:
 
         return {'image_file': image, 'data': data}
 
+
+class Crop:
+    def __init__(self, cropped_size):
+        self.cropped_size = cropped_size
+
+    def __call__(self, data4net):
+        image, data = data4net['image_file'], data4net['data']
+
+        # Generate random shift.
+        shift_x = random.randint(0, image.shape[1] - self.cropped_size[1])
+        shift_y = random.randint(0, image.shape[0] - self.cropped_size[0])
+
+        # Crop image. Shift is defined randomly.
+        image = image[shift_y:shift_y+self.cropped_size[0], shift_x:shift_x+self.cropped_size[1]]
+
+        # Shift rectangle.
+        for d in data:
+            if d['name'] == 'NoObject':
+                break
+            d['bndbox'].xmin -= shift_x
+            d['bndbox'].ymin -= shift_y
+            d['bndbox'].xmax -= shift_x
+            d['bndbox'].ymax -= shift_y
+
+        return {'image_file': image, 'data': data}
